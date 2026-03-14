2ef77eead6b65374d4000c5ee3a09292d37676d8
subiksha270406 <msubiksha27@gmail.com>
2026-03-14 12:00:19 +0530

    Deployment setup

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,8 +1,10 @@
 import axios from 'axios';
 
-const API = axios.create({
-    baseURL: 'http://localhost:5000/api',
-});
+// const API = axios.create({
+//     baseURL: 'http://localhost:5000/api',
+// });
+
+const API = axios.create({ baseURL: 'https://online-issue-resolution-tracker.onrender.com' });
 
 // Automatically add the Token to every request if it exists
 API.interceptors.request.use((req) => {
